de9e94dfb2adfe5c19248b22b03dc4bd33fbe380
LinearTransformation <dominhquang_t67@hus.edu.vn>
2023-10-31 07:23:34 +0700

    Add Mario

---
+++ Mario/Map/level.py
@@ -0,0 +1,5 @@
+import pygame
+
+class Level:
+    def __init__(self, level_data, surface):
+
---
+++ Mario/Map/settings.py
@@ -0,0 +1,4 @@
+vertical_tile_number = 11 #chiều cao của vùng ta có thể dựng map trong đó
+tile_size = 64 #tile_size ta đã chọn ở Tile
+screen_height = vertical_tile_number * tile_size
+screen_width = 1200
